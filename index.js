3e0511bd6d276f4933f76856321b837e3846208d
Brian De Maio <bdemaio1@gmail.com>
2019-12-08 22:48:42 -0500

    Adding in a second window to add items in

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 //Listen for app to be ready
 app.on('ready', () => {
@@ -17,6 +18,11 @@ app.on('ready', () => {
         slashes: true
     }));
 
+    //Quit app when closed
+    mainWindow.on('closed', () => {
+        app.quit();
+    });
+
     //Build menu from template
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
 
@@ -24,11 +30,36 @@ app.on('ready', () => {
     Menu.setApplicationMenu(mainMenu);
 });
 
+//Create window for adding items
+function createAddWindow() {
+    //Create new window
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add Shopping List Item'
+    });
+
+    //Load html file into window
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    //Set addWindow to null when closed
+    addWindow.on('close', () => {
+        addWindow = null;
+    });
+}
+
 //Create menu template
 const mainMenuTemplate = [{
     label: 'File',
     submenu: [{
-        label: 'Add Item'
+        label: 'Add Item',
+        click() {
+            createAddWindow();
+        }
     }, {
         label: 'Clear Items'
     }, {
