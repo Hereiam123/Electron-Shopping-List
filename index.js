cf9b244e868f224dce8d6a0baec950c7217d1274
Brian De Maio <bdemaio1@gmail.com>
2019-12-08 22:41:53 -0500

    Adding in menus for application toolbar

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
@@ -13,8 +13,29 @@ app.on('ready', () => {
     //Load html file into window
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'firstWindow.html'),
-        protocol: "file:",
+        protocol: 'file:',
         slashes: true
     }));
+
+    //Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+    //Insert menu
+    Menu.setApplicationMenu(mainMenu);
 });
 
+//Create menu template
+const mainMenuTemplate = [{
+    label: 'File',
+    submenu: [{
+        label: 'Add Item'
+    }, {
+        label: 'Clear Items'
+    }, {
+        label: 'Quit',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click() {
+            app.quit();
+        }
+    }]
+}];
